8a419ee5f294ddd10f7e712c4186f2da562bfd36
Leandro Souza <Leandro.Souza@dominos.co.uk>
2018-04-19 11:51:21 +0100

    Fix for Analytics on iOS

--- src/microsoft-appcenter.ios.ts
+++ src/microsoft-appcenter.ios.ts
@@ -1,5 +1,47 @@
-import { Common } from './microsoft-appcenter.common';
+import { Common, IAppCenterConfiguration } from './microsoft-appcenter.common';
 
-export class Appcenter extends Common {
+export class AppCenter extends Common {
+    
+    public static init(config: IAppCenterConfiguration): void {
+        // Not implemented yet
+    }
 
+    public static getInstallId(): string {
+        return "not implemented yet";
+    }
+
+    public static isEnabled(): boolean {
+        return false;
+    }
+
+    public static disable(): void {
+        // Not implemented yet
+    }
+}
+
+export class Analytics {
+    public static disable(): void {
+        // Not implemented yet
+    }
+
+    public static enable(): void {
+        // Not implemented yet
+    }
+
+    public static getClass(): any {
+        return "not implemented yet";
+    }
+
+    public static isEnabled(): boolean {
+        return false;
+    }
+
+    public static trackEvent(eventName: string, properties?: HashMap[]): void {
+        // Not implemented yet
+    }
+}
+
+export interface HashMap {
+    key: string;
+    value: string;
 }
